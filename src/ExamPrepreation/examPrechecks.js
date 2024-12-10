7910c785c19d69b215ad8a6ae0945ef43e1d913e
Faraz <mohsin.waheed@gitlogix.com>
2024-12-10 16:10:15 +0500

    resolve conflict

--- src/ExamPrepreation/examPrechecks.js
+++ src/ExamPrepreation/examPrechecks.js
@@ -7,13 +7,14 @@ import { IdentityVerificationScreenThree } from './identityVerificationScreenThr
 import { IdentityVerificationScreenFour } from './identityVerificationScreenFour';
 import { IdentityVerificationScreenFive } from './IdentityVerificationScreenFive';
 import { ExamPreparation } from './examPreprationScreen';
-import { addSectionSessionRecord, cleanupZendeskWidget, convertDataIntoParse, getSecureFeatures, handlePreChecksRedirection, loadZendeskWidget, registerEvent, updatePersistData, updateThemeColor } from '../utils/functions';
+import { addSectionSessionRecord, cleanupZendeskWidget, convertDataIntoParse, getSecureFeatures, handlePreChecksRedirection, loadZendeskWidget, logger, normalizeLanguage, registerEvent, updatePersistData, updateThemeColor } from '../utils/functions';
 import { PrevalidationInstructions } from './PrevalidationInstructions';
 import { ASSET_URL, languages, preChecksSteps, prevalidationSteps, systemDiagnosticSteps } from '../utils/constant';
 import { MobileProctoring } from './mobileProctoring';
 import 'notyf/notyf.min.css';
 // import Talk from 'talkjs';
 // import interact from 'interactjs';
+
 
 const modal = document.createElement('div');
 modal.className = 'modal';
@@ -63,15 +64,10 @@ const PrevalidationinstructionContainer = document.createElement('div');
 PrevalidationinstructionContainer.className = 'tab-content';
 PrevalidationinstructionContainer.id = 'Prevalidationinstruction';
 
-const identitySteps = document.createElement('div');
-identitySteps.className = 'steps-container';
-identitySteps.id = 'modal-steps-container';
+const mobileProctingContainer = document.createElement('div');
+mobileProctingContainer.className = 'tab-content';
+mobileProctingContainer.id = 'MobileProctoring';
 
-const mobileProctingConatiner = document.createElement('div');
-identitySteps.className = 'mobile-procting-conatiner';
-identitySteps.id = 'mobileProctingConatiner';
-
-tabContentsWrapper.appendChild(identitySteps);
 tabContentsWrapper.appendChild(ExamPreparationContainer);
 tabContentsWrapper.appendChild(SystemDiagnosticsContainer);
 tabContentsWrapper.appendChild(IdentityVerificationScreenOneContainer);
@@ -80,7 +76,7 @@ tabContentsWrapper.appendChild(IdentityVerificationScreenThreeContainer);
 tabContentsWrapper.appendChild(IdentityVerificationScreenFourContainer);
 tabContentsWrapper.appendChild(IdentityVerificationScreenFiveContainer);
 tabContentsWrapper.appendChild(PrevalidationinstructionContainer);
-tabContentsWrapper.appendChild(mobileProctingConatiner);
+tabContentsWrapper.appendChild(mobileProctingContainer);
 
 // const initializeLiveChat = () => {
 // 	const isLoggedIn = !!localStorage.getItem('mereosToken');
@@ -176,9 +172,7 @@ tabContentsWrapper.appendChild(mobileProctingConatiner);
 // 		interact(element).draggable({
 // 			inertia: true,
 // 			listeners: {
-// 				start(event) {
-// 					console.log('Drag started', event);
-// 				},
+// 			
 // 				move(event) {
 // 					const { target } = event;
 
@@ -193,44 +187,34 @@ tabContentsWrapper.appendChild(mobileProctingConatiner);
 // 					target.setAttribute('data-x', x);
 // 					target.setAttribute('data-y', y);
 // 				},
-// 				end(event) {
-// 					console.log('Drag ended', event);
-// 				},
+// 
 // 			},
 // 		});
 // 	}
 // };
 
+// const shadowRoot = modalContent.attachShadow({ mode: 'open' });
 modalContent.appendChild(tabContentsWrapper);
-const schoolTheme = localStorage.getItem('schoolTheme') !== undefined ? JSON.parse(localStorage.getItem('schoolTheme')) : {};
+// const link = document.createElement('link');
+// link.rel = 'stylesheet';
+// link.href = '../assets/css/modal.css';
+// shadowRoot.appendChild(link);
 
 const navigate = (newTabId) => {
 	showTab(newTabId);
 };
 
-const openModal = (callback) => {
-	console.log = function() {};
-	document.body.appendChild(modal);
-	modal.style.display = 'block';
-
-	const activeTab = handlePreChecksRedirection(callback);
-	const preChecksStep = JSON.parse(localStorage.getItem('preChecksSteps'));
-
-	if (preChecksStep === null) {
-		localStorage.setItem('preChecksSteps', JSON.stringify(preChecksSteps));
-	}
-	showTab(activeTab, callback);
-	const session = convertDataIntoParse('session');
-	startSession(session);
-
+const createLanguageDropdown = () => {
 	const modalHeader = document.createElement('div');
 	modalHeader.className = 'header';
 
 	const languageContainer = document.createElement('section');
 	languageContainer.className = 'dropdown';
 
+	const schoolTheme = localStorage.getItem('schoolTheme') !== undefined ? JSON.parse(localStorage.getItem('schoolTheme')) : {};
 	const defaultLanguage = schoolTheme?.language || 'en';
-	let selectedLanguage = languages.find(lang => lang.keyword === defaultLanguage);
+	const filterLanguage = normalizeLanguage(defaultLanguage);
+	let selectedLanguage = languages.find(lang => lang.keyword === filterLanguage.trim());
 
 	const selectContainer = document.createElement('div');
 	selectContainer.className = 'select';
@@ -239,6 +223,7 @@ const openModal = (callback) => {
 	const flagImg = document.createElement('img');
 	flagImg.src = selectedLanguage.src;
 	flagImg.alt = selectedLanguage.alt;
+	flagImg.className = 'flag-icon-img';
 	selectContainer.appendChild(flagImg);
 
 	const label = document.createElement('label');
@@ -258,16 +243,19 @@ const openModal = (callback) => {
 	languages.forEach((lang, index) => {
 		const optionDiv = document.createElement('div');
 		optionDiv.key = index;
+
 		optionDiv.className = `dropdown-option ${lang.keyword === selectedLanguage.keyword ? 'selected' : ''}`;
 		optionDiv.onclick = () => onTrigger(lang);
 
 		const optionFlag = document.createElement('img');
 		optionFlag.src = lang.src;
 		optionFlag.alt = lang.alt;
+		optionFlag.className = 'flag-icon-img';
 		optionDiv.appendChild(optionFlag);
 
 		const optionText = document.createElement('div');
 		optionText.className = 'text';
+		optionText.id='dropdown-text-language';
 		optionText.textContent = i18next.t(lang.value);
 		optionDiv.appendChild(optionText);
 
@@ -286,28 +274,36 @@ const openModal = (callback) => {
 				flagImg.src = selectedLang.src;
 				label.textContent = i18next.t(selectedLang.value);
 	
-				languages.forEach(lang => {
-					const optionDiv = Array.from(languageDropdown.children).find(
-						option => option.textContent.trim() === i18next.t(lang.value)
-					);
+				languages.forEach((lang,index) => {
+					const optionDiv = languageDropdown.children[index];
 					if (optionDiv) {
 						const optionText = optionDiv.querySelector('.text');
 						optionText.textContent = i18next.t(lang.value);
 					}
 				});
 			})
-			.catch(err => console.error(err));
+			.catch(err => logger.error(err));
 	
 		languageDropdown.classList.remove('active');
 		updatePersistData('schoolTheme', { language: selectedLang.keyword });
 	}
 };
 
-export const setLanguage = (lang) => {
-	i18next.changeLanguage(lang, (err) => {
-		if (err) return console.error(err);
-		updateTranslations();
-	});
+const openModal = (callback) => {
+	document.body.appendChild(modal);
+	modal.style.display = 'block';
+
+	const activeTab = handlePreChecksRedirection(callback);
+	const preChecksStep = JSON.parse(localStorage.getItem('preChecksSteps'));
+
+	if (preChecksStep === null) {
+		localStorage.setItem('preChecksSteps', JSON.stringify(preChecksSteps));
+	}
+	showTab(activeTab, callback);
+	const session = convertDataIntoParse('session');
+	startSession(session);
+
+	createLanguageDropdown();
 };
 
 function closeModal() {
@@ -329,6 +325,8 @@ const showTab = async (tabId, callback) => {
 	try {
 		loadZendeskWidget();
 		// initializeLiveChat();
+
+		initializeI18next();
 
 		updateThemeColor();
 		const tabs = document.querySelectorAll('.tab');
@@ -389,7 +387,7 @@ const showTab = async (tabId, callback) => {
 			await IdentityVerificationScreenThree(IdentityVerificationScreenThreeContainer);
 		} else if (tabId === 'IdentityVerificationScreenFour') {
 			if (!secureFeatures?.find(entity => entity.key === 'record_room')) {
-				navigate('IdentityVerificationScreenFive');
+				navigate('MobileProctoring');
 				return;
 			}
 			await IdentityVerificationScreenFour(IdentityVerificationScreenFourContainer);
@@ -398,7 +396,7 @@ const showTab = async (tabId, callback) => {
 				navigate('IdentityVerificationScreenFive');
 				return;
 			}
-			await MobileProctoring(mobileProctingConatiner);
+			await MobileProctoring(mobileProctingContainer);
 		} else if (tabId === 'IdentityVerificationScreenFive') {
 			if (!secureFeatures?.find(entity => entity.key === 'record_screen')) {
 				closeModal(callback);
@@ -411,7 +409,7 @@ const showTab = async (tabId, callback) => {
 			return;
 		}
 	} catch (e) {
-		console.error('error in showTab', e);
+		logger.error('error in showTab', e);
 	}
 };
 
@@ -430,7 +428,7 @@ const startSession = async (session) => {
 				sessionStatus: 'Initiated'
 			});
 	} catch (e) {
-		console.log('error', e);
+		logger.error('Error in start Session', e);
 	}
 };
 
@@ -439,41 +437,46 @@ export const updateTranslations = () => {
 	showTab(activeTab,window.globalCallback);
 };
 
-i18next.init({
-	lng: schoolTheme?.language.split('-')[0] || 'en',
-	resources: {
-		en: {
-			translation: require('../assets/locales/en/translation.json')
-		},
-       
-		fr: {
-			translation: require('../assets/locales/fr/translation.json')
-		},
-		it: {
-			translation: require('../assets/locales/it/translation.json')
-		},
-		pt: {
-			translation: require('../assets/locales/pt/translation.json')
-		},
-		nl: {
-			translation: require('../assets/locales/nl/translation.json')
-		},
-		es: {
-			translation: require('../assets/locales/es/translation.json')
-		},
-		de: {
-			translation: require('../assets/locales/de/translation.json')
-		},
-       
-	}
-}, (err) => {
-	if (err) return console.error(err);
-	updateTranslations();
-});
+const initializeI18next = () => {
+	if (i18next.isInitialized) return;
 
-document.addEventListener('DOMContentLoaded', () => {
-	updateTranslations();
-});
+	const schoolLanguage = localStorage.getItem('schoolTheme') !== undefined ? JSON.parse(localStorage.getItem('schoolTheme')) : {};
+	const defaultLanguage = schoolLanguage?.language || 'en';
+
+	i18next.init({
+		lng: normalizeLanguage(defaultLanguage),
+		resources: {
+			en: {
+				translation: require('../assets/locales/en/translation.json')
+			}, 
+			fr: {
+				translation: require('../assets/locales/fr/translation.json')
+			},
+			it: {
+				translation: require('../assets/locales/it/translation.json')
+			},
+			pt: {
+				translation: require('../assets/locales/pt/translation.json')
+			},
+			nl: {
+				translation: require('../assets/locales/nl/translation.json')
+			},
+			es: {
+				translation: require('../assets/locales/es/translation.json')
+			},
+			de: {
+				translation: require('../assets/locales/de/translation.json')
+			},	 
+		}
+	}, (err) => {
+		if (err) return logger.error('Error in language', err);
+		updateTranslations();
+	});
+	
+	document.addEventListener('DOMContentLoaded', () => {
+		updateTranslations();
+	});
+};
 
 function checkToken() {
 	if (!localStorage.getItem('mereosToken')) {
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -95,4 +95,340 @@ export const languages = [
 	{name: 'French', value: 'french', src: `${ASSET_URL}/flag-of-france.svg`, alt: '', keyword: 'fr' },
 	{name: 'Portuguese (Brazil)', value: 'portuguese_brazil', src: `${ASSET_URL}/flag-of-brazil.svg`, alt: '', keyword: 'pt' },
 	{name: 'Italian', value: 'italian', src: `${ASSET_URL}/flag-of-italy.svg`, alt: '', keyword: 'it' },
+	{name: 'Dutch', value: 'dutch', src: `${ASSET_URL}/netherlands.svg`, alt: '', keyword: 'nl' },
 ];
+
+export const examPreprationCss = `.exam-preparation {
+  display: flex;
+  position: relative;
+  margin: 10px;
+  white-space: pre-line;
+}
+
+.exam-preparation h1 {
+  text-align: center;
+}
+
+@media (min-width: 401px) {
+  .exam-preparation h1 {
+      text-align: left;
+  }
+}
+
+.exam-preparation .img1 {
+  position: absolute;
+  top: 60%;
+  right: 15%;
+  transform: rotate(25.43deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img1 {
+      top: 63%;
+      right: 14%;
+      width: 62px;
+  }
+}
+
+.exam-preparation .img2 {
+  position: absolute;
+  top: 15%;
+  right: 20%;
+  transform: rotate(-17.18deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img2 {
+      top: -7%;
+      right: 0%;
+  }
+}
+
+.exam-preparation .img3 {
+  position: absolute;
+  top: 15%;
+  left: 25%;
+  transform: rotate(-16.25deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img3 {
+      top: -3%;
+      left: 25%;
+      width: 78px;
+  }
+}
+
+.exam-preparation .img4 {
+  position: absolute;
+  top: 70%;
+  left: 5%;
+  transform: rotate(11.39deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img4 {
+      top: 62%;
+  }
+}
+
+.exam-preparation .img5 {
+  position: absolute;
+  top: 35%;
+  right: 10%;
+  transform: rotate(-165deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img5 {
+      top: 15%;
+      right: 0%;
+      width: 78px;
+  }
+}
+
+.exam-preparation .img6 {
+  position: absolute;
+  top: 85%;
+  right: 15%;
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img6 {
+      display: none;
+  }
+}
+
+.exam-preparation .img7 {
+  position: absolute;
+  top: 85%;
+  left: 15%;
+  transform: rotate(-69.35deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img7 {
+      display: none;
+  }
+}
+
+.exam-preparation .img8 {
+  position: absolute;
+  top: 25%;
+  left: 10%;
+  transform: rotate(-69.35deg);
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .img8 {
+      top: 15%;
+      left: -5%;
+      width: 78px;
+  }
+}
+
+.exam-preparation .exam-preparation-container {
+  margin: 25px auto 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.exam-preparation .exam-preparation-container .header-img {
+  width: fit-content;
+}
+
+.exam-preparation .exam-preparation-container .ep-msg {
+  font-size: 16px;
+  line-height: 160%;
+  text-align: center;
+  color: #8D979F;
+  width: 650px;
+}
+
+@media (max-width: 720px) {
+  .exam-preparation .exam-preparation-container .ep-msg {
+      width: 98%;
+  }
+}
+
+.exam-preparation .exam-preparation-container > h1 {
+  font-weight: 500;
+  margin-top: 5px;
+}
+
+.exam-preparation .exam-preparation-container label {
+  font-weight: 400;
+  color: #92959A;
+}
+`;
+
+export const modalCss = `@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+body {
+  font-family: 'Poppins', sans-serif;
+  font-style: var(--font-style);
+}
+:root {
+  --theme-color: #FF961B;
+  --font-style: var(--font-style);
+}
+
+.modal {
+  display: none;
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+  overflow: auto;
+  background-color: rgba(0, 0, 0, 0.5);
+}
+.modal-content {
+  display: flex;
+  flex-direction: column;
+  width: 830px;
+  position: fixed;
+  top: 50%;
+  height: 530px;
+  left: 50%;
+  z-index: 2147483647;
+  transform: translate(-50%, -50%);
+  background: #FFFFFF;
+  box-shadow: 0px 10px 50px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  font-size: 1.1rem;
+  padding: 25px;
+  overflow: auto;
+  padding-top: 10px;
+}
+.dropdown-option{
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  gap: 20px;
+  margin-top: 12px;
+  padding-left: 12px;
+  padding-bottom: 5px;
+}
+
+.flag-icon-img{
+  width: 25px;
+}
+.dropdown-content .text{
+  font-size: 14px !important;
+  font-weight: 400 !important;
+  cursor: pointer;
+}
+.close {
+  color: #aaa;
+  font-size: 28px;
+  font-weight: bold;
+  display: flex;
+  cursor: pointer;
+  align-items: end;
+  justify-content: end;
+  position: absolute;
+  right: 13px;
+  top: 2px;
+}
+
+.close:hover,
+.close:focus {
+  color: black;
+  text-decoration: none;
+  cursor: pointer;
+}
+
+.tab {
+  cursor: pointer;
+  padding: 10px;
+  display: inline-block;
+}
+
+.tab.active {
+  color: var(--theme-color);
+}
+
+.tab-content {
+  display: none;
+}
+
+.tab-content.active {
+  display: block;
+}
+
+.tabs-container > div{
+  white-space: nowrap;
+  font-size: 15px;
+}
+
+.header {
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  margin-bottom: 10px;
+}
+
+.dropdown {
+  position: relative;
+  cursor: pointer;
+  display: inline-flex;
+  align-items: center;
+  margin-left: 10px;
+  cursor: pointer;
+}
+.live-chat-container{
+  position: fixed;
+  bottom: 87px;
+  right: 33px;
+  height: 500px;
+  z-index: 99999999;
+}
+
+.dropdown .select {
+  display: flex;
+  align-items: center;
+  padding: 5px;
+  column-gap: 8px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  cursor: pointer;
+  max-width: 182px;
+  justify-content: space-between;
+  z-index: 99999;
+}
+.dropdown .select label{
+  cursor: pointer;
+  font-size: 15px;
+}
+
+.dropdown .select div {
+  margin-right: 5px;
+}
+
+.dropdown-content {
+  position: absolute;
+  top: 42px;
+  right: 0;
+  background-color: white;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 4px;
+  overflow: hidden;
+  display: none;
+}
+
+.dropdown-content.active {
+  display: block;
+  z-index: 999;
+}
+
+.dropdown .option {
+  padding: 8px 12px;
+  cursor: pointer;
+}
+
+.dropdown .option:hover {
+  background-color: #f0f0f0;
+}`;

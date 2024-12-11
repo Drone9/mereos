178230c90c99d272fe0cb76f146fb76c0c46a005
Umar Toor <umar.toor@mereos.eu>
2024-12-11 15:40:33 +0500

    [1.1.1] BaseUrl is reverted

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,8 @@
 export const ASSET_URL = 'https://mereos-corder.s3.eu-west-3.amazonaws.com/library_icons';
 
-export const BASE_URL = 'https://corder-api.mereos.eu';
+export const BASE_URL = process.env.NODE_ENV === 'production'
+	? 'https://corder-api.mereos.eu'
+	: 'https://corder-api.mereos-datasafe.com';
 
 export const SOCKET_URL = 'wss://mobile-socket.mereos.eu/websocket/';
 

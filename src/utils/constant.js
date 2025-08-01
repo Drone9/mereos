211dc9c93fc8bac205f64392248f99008f48712e
Umar Toor <umar.toor@mereos.eu>
2025-08-01 11:17:10 +0500

    [1.1.6] Version 1.1.6 is released

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,6 +1,6 @@
 export const ASSET_URL = 'https://mereos-corder.s3.eu-west-3.amazonaws.com/library_icons';
 
-export const BASE_URL = 'https://corder-api.mereos-datasafe.com/v1.1.0/';
+export const BASE_URL = 'https://corder-api.mereos.eu/v1.1.1/';
 
 export const SOCKET_URL = 'wss://mobile-socket.mereos.eu/websocket/';
 
